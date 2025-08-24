43a748c2ee2349402b3bb412012a69589dcd6d9d
ANDRINOUNO1 <mataandrewczar@gmail.com>
2025-08-24 22:39:43 +0800

    waw

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const errorHandler = require('./_middleware/error-handler');
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:4200', // Angular dev server default port
+  origin: ['http://localhost:5173', 'http://localhost:3000'], // Allow multiple origins
   credentials: true
 }));
 app.use(express.json());
